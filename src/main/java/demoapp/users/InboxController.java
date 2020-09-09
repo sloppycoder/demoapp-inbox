8d99f981e0be7a347301f967ab13ea5badc3b887
Li Lin <guru.lin@gmail.com>
2020-09-10 00:58:07 +0800

    implemented SSE.

--- src/main/java/demoapp/users/InboxController.java
+++ src/main/java/demoapp/users/InboxController.java
@@ -8,11 +8,15 @@ import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
@@ -31,18 +35,37 @@ public class InboxController {
   public Mono<Integer> getInboxUnreadCount(@PathVariable("userId") String userId) {
     log.info("retrieving unread count for user {}", userId);
 
-    return mongo.query(Inbox.class)
-            .matching(query(where("userId").is(userId)))
-            .first()
-      .flatMap(
-            inbox -> Mono.just(inbox.getUnreadCount())
-      ).switchIfEmpty(
-            Mono.error(new InboxNotFoundException())
-      );
+    return getInboxForUser(userId)
+            .flatMap(
+                inbox -> Mono.just(inbox.getUnreadCount())
+            ).switchIfEmpty(
+                Mono.error(new InboxNotFoundException())
+            );
+  }
+
+  @RequestMapping(value = "/inbox/{userId}/unread-sse")
+  public Flux<ServerSentEvent<Integer>> getInboxUnreadCountStream(
+          @PathVariable("userId") String userId,
+          @RequestParam(defaultValue = "2") int iter) {
+    log.info("retrieving unread count stream for user {} for {} iterations", userId, iter);
+
+    return getInboxForUser(userId)
+            .repeat(iter)
+            .delayElements(Duration.ofSeconds(1))
+            .map(inbox -> ServerSentEvent.<Integer> builder()
+                    .event("unread-count-event")
+                    .data(inbox.getUnreadCount())
+                    .build());
   }
 
   @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "not found")
   @ExceptionHandler(InboxNotFoundException.class)
   public void notFoundHandler() {
+  }
+
+  private Mono<Inbox>  getInboxForUser(String userId) {
+    return mongo.query(Inbox.class)
+            .matching(query(where("userId").is(userId)))
+            .first();
   }
 }
--- src/test/java/demoapp/users/InboxTests.java
+++ src/test/java/demoapp/users/InboxTests.java
@@ -17,6 +17,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InboxTests {
@@ -41,21 +42,37 @@ class InboxTests {
 		Integer count = response.bodyToMono(Integer.class).block();
 
 		assertEquals(response.statusCode(), HttpStatus.OK);
-		assertEquals(count, 1);
+		assertEquals(count, 2);
+	}
 
+
+	@Test
+	@Order(3)
+	void can_get_unread_count_stream() {
+		WebClient client = WebClient.builder().baseUrl(getUrl("/2222/unread-sse?iter=1")).build();
+		ClientResponse response = client.get().exchange().block();
+		String body = response.bodyToMono(String.class).block();
+
+		assertEquals(response.statusCode(), HttpStatus.OK);
+		assertTrue(body.contains("event:unread-count-event"));
+		assertTrue(body.contains("data:1"));
 	}
 
 	private List<Inbox> testData() {
-		Inbox inbox = new Inbox(null, "1111", 1,
+		Inbox inbox = new Inbox(null, "1111", 2,
 				List.of(
 						newMessage("1|subject 1|this is not how it works"),
 						newMessage("2|subject 2|this is absolutely how it works")
 						));
-		return List.of(inbox);
+		Inbox inbox2 = new Inbox(null, "2222", 1,
+				List.of(
+						newMessage("1|on subject|are you hungry?")
+				));
+		return List.of(inbox, inbox2);
 	}
 
 	private Message newMessage(String line) {
-		String[] str = line.split("|");
+		String[] str = line.split("\\|");
 		return new Message(str[0], str[1], str[2], false);
 	}
 
